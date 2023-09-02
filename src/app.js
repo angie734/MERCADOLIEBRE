e79b3b972642f89cdd530fae0d086357af95014b
angie734 <43140244@fi.unju.edu.ar>
2023-09-01 22:42:08 -0300

    subiendo reformas

--- src/app.js
+++ src/app.js
@@ -4,16 +4,17 @@ const path = require("path");
 
 //para decirle donde se encuentran nuestro archivos estaticos
 app.use(express.static("public"));
-const port= process.env.PORT || 3002;
-app.listen(port,()=> "Servidor escuchando en el puerto 3002");
+const port= process.env.PORT || 3003;
+app.listen(port,()=> "Servidor escuchando en el puerto 3003");
 
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/home.html"));
 });
-app.get("/contacto",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./views/contacto.html"));
-});
+
 app.get("/registro",(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/registro.html"));
+});
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/login.html"));
 });
